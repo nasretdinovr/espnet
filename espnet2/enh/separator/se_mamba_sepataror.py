7ba282d1a029108664f7e8b02a4ab21d037985be
Rauf <rnasretdinov@nvidia.com>
2024-08-16 11:27:14 -0700

    added feature to adjust mamba_layers and mamba_blocks params through config

--- espnet2/enh/separator/se_mamba_sepataror.py
+++ espnet2/enh/separator/se_mamba_sepataror.py
@@ -28,6 +28,18 @@ else:
 class SEMambaSeparator(AbsSeparator):
     """Offline SEMambaSeparator.
 
+    On top of TFGridNetV2, TFGridNetV3 slightly modifies the internal architecture
+    to make the model sampling-frequency-independent (SFI). This is achieved by
+    making all network layers independent of the input time and frequency dimensions.
+
+    Reference:
+    [1] Z.-Q. Wang, S. Cornell, S. Choi, Y. Lee, B.-Y. Kim, and S. Watanabe,
+    "TF-GridNet: Integrating Full- and Sub-Band Modeling for Speech Separation",
+    in TASLP, 2023.
+    [2] Z.-Q. Wang, S. Cornell, S. Choi, Y. Lee, B.-Y. Kim, and S. Watanabe,
+    "TF-GridNet: Making Time-Frequency Domain Models Great Again for Monaural
+    Speaker Separation", in ICASSP, 2023.
+
     NOTES:
     As outlined in the Reference, this model works best when trained with variance
     normalized mixture input and target, e.g., with mixture of shape [batch, samples,
@@ -65,7 +77,8 @@ class SEMambaSeparator(AbsSeparator):
         input_dim,
         n_srcs=2,
         n_imics=1,
-        n_layers=6,
+        mamba_blocks=6,
+        mamba_layers=6,
         lstm_hidden_units=192,
         attn_n_head=4,
         attn_qk_output_channel=4,
@@ -77,7 +90,6 @@ class SEMambaSeparator(AbsSeparator):
     ):
         super().__init__()
         self.n_srcs = n_srcs
-        self.n_layers = n_layers
         self.n_imics = n_imics
         assert self.n_imics == 1, self.n_imics
 
@@ -107,12 +119,20 @@ class SEMambaSeparator(AbsSeparator):
         self.deconv = nn.ConvTranspose2d(emb_dim, n_srcs * 2, ks, padding=padding)
         """
         # SEMamba
-        self.num_tscblocks = 4
+        #self.num_tscblocks = 4
+        #self.num_tscblocks = 14
+        #self.num_tscblocks = 10
+        #self.num_tscblocks = 20
+        self.num_tscblocks = mamba_blocks
         self.dense_encoder = DenseEncoder(in_channel=2)
 
         self.TSMamba = nn.ModuleList([])
         for i in range(self.num_tscblocks):
-            self.TSMamba.append(TSMambaBlock())
+            self.TSMamba.append(TSMambaBlock(mamba_layers))
+
+        #self.laynorms = nn.ModuleList([])
+        #for i in range(self.num_tscblocks):
+        #    self.laynorms.append(RMSNorm(64))
         
         self.mask_decoder = MaskDecoder(out_channel=1)
         self.phase_decoder = PhaseDecoder(out_channel=1)
@@ -167,22 +187,11 @@ class SEMambaSeparator(AbsSeparator):
         return batch, ilens, OrderedDict()
         """
 
-        #spectrum = rearrange(input, 'B T F -> B F T')
+        noisy_mag = torch.abs(input)
+        noisy_pha = torch.angle(input)
+        noisy_mag = torch.pow(noisy_mag, 0.3)
 
-        #noisy_mag = torch.abs(spectrum)
-        #noisy_pha = torch.angle(spectrum)
-        #noisy_mag = torch.pow(noisy_mag, 0.3)
 
-        real_part = input.real
-        imag_part = input.imag
-        noisy_mag = torch.sqrt( real_part.pow(2) + imag_part.pow(2))
-        noisy_pha = torch.atan2( imag_part, real_part )
-        noisy_mag = torch.pow( noisy_mag, 0.3 )
-        
-        #print(noisy_mag.shape) # [B, T, F]
-
-        #noisy_mag = noisy_mag.unsqueeze(-1).permute(0, 3, 2, 1) # [B, 1, T, F]
-        #noisy_pha = noisy_pha.unsqueeze(-1).permute(0, 3, 2, 1) # [B, 1, T, F]
         noisy_mag = noisy_mag.unsqueeze(1)  # [B, 1, T, F]
         noisy_pha = noisy_pha.unsqueeze(1)  # [B, 1, T, F]
         x = torch.cat((noisy_mag, noisy_pha), dim=1) # [B, 2, T, F]
@@ -197,7 +206,6 @@ class SEMambaSeparator(AbsSeparator):
         for i in range(self.num_tscblocks):
             x = self.TSMamba[i](x)
         
-        #denoised_mag = (noisy_mag * self.mask_decoder(x)).permute(0, 3, 2, 1).squeeze(-1)
         denoised_mag = (self.mask_decoder(x)).permute(0, 3, 2, 1).squeeze(-1)
         denoised_pha = self.phase_decoder(x).permute(0, 3, 2, 1).squeeze(-1)
         denoised_mag = torch.pow(denoised_mag, (1.0/0.3))
@@ -214,197 +222,6 @@ class SEMambaSeparator(AbsSeparator):
     def num_spk(self):
         return self.n_srcs
 
-
-class GridNetV3Block(nn.Module):
-    def __getitem__(self, key):
-        return getattr(self, key)
-
-    def __init__(
-        self,
-        emb_dim,
-        emb_ks,
-        emb_hs,
-        hidden_channels,
-        n_head=4,
-        qk_output_channel=4,
-        activation="prelu",
-        eps=1e-5,
-    ):
-        super().__init__()
-        assert activation == "prelu"
-
-        in_channels = emb_dim * emb_ks
-
-        self.intra_norm = nn.LayerNorm(emb_dim, eps=eps)
-        self.intra_rnn = nn.LSTM(
-            in_channels, hidden_channels, 1, batch_first=True, bidirectional=True
-        )
-        if emb_ks == emb_hs:
-            self.intra_linear = nn.Linear(hidden_channels * 2, in_channels)
-        else:
-            self.intra_linear = nn.ConvTranspose1d(
-                hidden_channels * 2, emb_dim, emb_ks, stride=emb_hs
-            )
-
-        self.inter_norm = nn.LayerNorm(emb_dim, eps=eps)
-        self.inter_rnn = nn.LSTM(
-            in_channels, hidden_channels, 1, batch_first=True, bidirectional=True
-        )
-        if emb_ks == emb_hs:
-            self.inter_linear = nn.Linear(hidden_channels * 2, in_channels)
-        else:
-            self.inter_linear = nn.ConvTranspose1d(
-                hidden_channels * 2, emb_dim, emb_ks, stride=emb_hs
-            )
-
-        # use constant E not to be dependent on the number of frequency bins
-        E = qk_output_channel
-        assert emb_dim % n_head == 0
-
-        self.add_module("attn_conv_Q", nn.Conv2d(emb_dim, n_head * E, 1))
-        self.add_module(
-            "attn_norm_Q",
-            AllHeadPReLULayerNormalization4DC((n_head, E), eps=eps),
-        )
-
-        self.add_module("attn_conv_K", nn.Conv2d(emb_dim, n_head * E, 1))
-        self.add_module(
-            "attn_norm_K",
-            AllHeadPReLULayerNormalization4DC((n_head, E), eps=eps),
-        )
-
-        self.add_module(
-            "attn_conv_V", nn.Conv2d(emb_dim, n_head * emb_dim // n_head, 1)
-        )
-        self.add_module(
-            "attn_norm_V",
-            AllHeadPReLULayerNormalization4DC((n_head, emb_dim // n_head), eps=eps),
-        )
-
-        self.add_module(
-            "attn_concat_proj",
-            nn.Sequential(
-                nn.Conv2d(emb_dim, emb_dim, 1),
-                get_layer(activation)(),
-                LayerNormalization(emb_dim, dim=-3, total_dim=4, eps=eps),
-            ),
-        )
-
-        self.emb_dim = emb_dim
-        self.emb_ks = emb_ks
-        self.emb_hs = emb_hs
-        self.n_head = n_head
-
-    def forward(self, x):
-        """GridNetV2Block Forward.
-
-        Args:
-            x: [B, C, T, Q]
-            out: [B, C, T, Q]
-        """
-        B, C, old_T, old_Q = x.shape
-
-        olp = self.emb_ks - self.emb_hs
-        T = (
-            math.ceil((old_T + 2 * olp - self.emb_ks) / self.emb_hs) * self.emb_hs
-            + self.emb_ks
-        )
-        Q = (
-            math.ceil((old_Q + 2 * olp - self.emb_ks) / self.emb_hs) * self.emb_hs
-            + self.emb_ks
-        )
-
-        x = x.permute(0, 2, 3, 1)  # [B, old_T, old_Q, C]
-        x = F.pad(x, (0, 0, olp, Q - old_Q - olp, olp, T - old_T - olp))  # [B, T, Q, C]
-
-        # intra RNN
-        input_ = x
-        intra_rnn = self.intra_norm(input_)  # [B, T, Q, C]
-        if self.emb_ks == self.emb_hs:
-            intra_rnn = intra_rnn.view([B * T, -1, self.emb_ks * C])  # [BT, Q//I, I*C]
-            intra_rnn, _ = self.intra_rnn(intra_rnn)  # [BT, Q//I, H]
-            intra_rnn = self.intra_linear(intra_rnn)  # [BT, Q//I, I*C]
-            intra_rnn = intra_rnn.view([B, T, Q, C])
-        else:
-            intra_rnn = intra_rnn.view([B * T, Q, C])  # [BT, Q, C]
-            intra_rnn = intra_rnn.transpose(1, 2)  # [BT, C, Q]
-            intra_rnn = F.unfold(
-                intra_rnn[..., None], (self.emb_ks, 1), stride=(self.emb_hs, 1)
-            )  # [BT, C*I, -1]
-            intra_rnn = intra_rnn.transpose(1, 2)  # [BT, -1, C*I]
-
-            intra_rnn, _ = self.intra_rnn(intra_rnn)  # [BT, -1, H]
-
-            intra_rnn = intra_rnn.transpose(1, 2)  # [BT, H, -1]
-            intra_rnn = self.intra_linear(intra_rnn)  # [BT, C, Q]
-            intra_rnn = intra_rnn.view([B, T, C, Q])
-            intra_rnn = intra_rnn.transpose(-2, -1)  # [B, T, Q, C]
-        intra_rnn = intra_rnn + input_  # [B, T, Q, C]
-
-        intra_rnn = intra_rnn.transpose(1, 2)  # [B, Q, T, C]
-
-        # inter RNN
-        input_ = intra_rnn
-        inter_rnn = self.inter_norm(input_)  # [B, Q, T, C]
-        if self.emb_ks == self.emb_hs:
-            inter_rnn = inter_rnn.view([B * Q, -1, self.emb_ks * C])  # [BQ, T//I, I*C]
-            inter_rnn, _ = self.inter_rnn(inter_rnn)  # [BQ, T//I, H]
-            inter_rnn = self.inter_linear(inter_rnn)  # [BQ, T//I, I*C]
-            inter_rnn = inter_rnn.view([B, Q, T, C])
-        else:
-            inter_rnn = inter_rnn.view(B * Q, T, C)  # [BQ, T, C]
-            inter_rnn = inter_rnn.transpose(1, 2)  # [BQ, C, T]
-            inter_rnn = F.unfold(
-                inter_rnn[..., None], (self.emb_ks, 1), stride=(self.emb_hs, 1)
-            )  # [BQ, C*I, -1]
-            inter_rnn = inter_rnn.transpose(1, 2)  # [BQ, -1, C*I]
-
-            inter_rnn, _ = self.inter_rnn(inter_rnn)  # [BQ, -1, H]
-
-            inter_rnn = inter_rnn.transpose(1, 2)  # [BQ, H, -1]
-            inter_rnn = self.inter_linear(inter_rnn)  # [BQ, C, T]
-            inter_rnn = inter_rnn.view([B, Q, C, T])
-            inter_rnn = inter_rnn.transpose(-2, -1)  # [B, Q, T, C]
-        inter_rnn = inter_rnn + input_  # [B, Q, T, C]
-
-        inter_rnn = inter_rnn.permute(0, 3, 2, 1)  # [B, C, T, Q]
-
-        inter_rnn = inter_rnn[..., olp : olp + old_T, olp : olp + old_Q]
-        batch = inter_rnn
-
-        Q = self["attn_norm_Q"](self["attn_conv_Q"](batch))  # [B, n_head, C, T, Q]
-        K = self["attn_norm_K"](self["attn_conv_K"](batch))  # [B, n_head, C, T, Q]
-        V = self["attn_norm_V"](self["attn_conv_V"](batch))  # [B, n_head, C, T, Q]
-        Q = Q.view(-1, *Q.shape[2:])  # [B*n_head, C, T, Q]
-        K = K.view(-1, *K.shape[2:])  # [B*n_head, C, T, Q]
-        V = V.view(-1, *V.shape[2:])  # [B*n_head, C, T, Q]
-
-        Q = Q.transpose(1, 2)
-        Q = Q.flatten(start_dim=2)  # [B', T, C*Q]
-
-        K = K.transpose(2, 3)
-        K = K.contiguous().view([B * self.n_head, -1, old_T])  # [B', C*Q, T]
-
-        V = V.transpose(1, 2)  # [B', T, C, Q]
-        old_shape = V.shape
-        V = V.flatten(start_dim=2)  # [B', T, C*Q]
-        emb_dim = Q.shape[-1]
-
-        attn_mat = torch.matmul(Q, K) / (emb_dim**0.5)  # [B', T, T]
-        attn_mat = F.softmax(attn_mat, dim=2)  # [B', T, T]
-        V = torch.matmul(attn_mat, V)  # [B', T, C*Q]
-
-        V = V.reshape(old_shape)  # [B', T, C, Q]
-        V = V.transpose(1, 2)  # [B', C, T, Q]
-        emb_dim = V.shape[1]
-
-        batch = V.contiguous().view(
-            [B, self.n_head * emb_dim, old_T, old_Q]
-        )  # [B, C, T, Q])
-        batch = self["attn_concat_proj"](batch)  # [B, C, T, Q])
-
-        out = batch + inter_rnn
-        return out
 
 
 class LayerNormalization(nn.Module):
@@ -469,6 +286,7 @@ def create_block(
     d_model, layer_idx=0, rms_norm=True, fused_add_norm=False, residual_in_fp32=False, 
     ):
     d_state = 16
+    #d_state = 32
     d_conv = 4
     expand = 4
     norm_epsilon = 0.00001
@@ -488,9 +306,8 @@ def create_block(
     return block
 
 class MambaBlock(nn.Module):
-    def __init__(self, in_channels):
+    def __init__(self, in_channels, n_layer):
         super(MambaBlock, self).__init__()
-        n_layer = 1
         self.forward_blocks  = nn.ModuleList( create_block(in_channels) for i in range(n_layer) )
         self.backward_blocks = nn.ModuleList( create_block(in_channels) for i in range(n_layer) )
 
@@ -517,54 +334,6 @@ class MambaBlock(nn.Module):
 
         return torch.cat([y_forward, y_backward], -1)
 
-
-"""
-class MambaBlock(nn.Module):
-    def __init__(self, in_channels, n_layer=1, bidirectional=False):
-        super(MambaBlock, self).__init__()
-        self.forward_blocks = nn.ModuleList([])
-        for i in range(n_layer):
-            self.forward_blocks.append(
-                Block(
-                    in_channels,
-                    mixer_cls=partial(Mamba, layer_idx=i, d_state=16, d_conv=4, expand=4),
-                    norm_cls=partial(RMSNorm, eps=1e-5),
-                    fused_add_norm=False,
-                )
-            )
-        if bidirectional:
-            self.backward_blocks = nn.ModuleList([])
-            for i in range(n_layer):
-                self.backward_blocks.append(
-                        Block(
-                        in_channels,
-                        mixer_cls=partial(Mamba, layer_idx=i, d_state=16, d_conv=4, expand=4),
-                        norm_cls=partial(RMSNorm, eps=1e-5),
-                        fused_add_norm=False,
-                    )
-                )
-
-        self.apply(partial(_init_weights, n_layer=n_layer))
-
-    def forward(self, input):
-        for_residual = None
-        forward_f = input.clone()
-        for block in self.forward_blocks:
-            forward_f, for_residual = block(forward_f, for_residual, inference_params=None)
-        residual = (forward_f + for_residual) if for_residual is not None else forward_f
-
-        if self.backward_blocks is not None:
-            back_residual = None
-            backward_f = torch.flip(input, [1])
-            for block in self.backward_blocks:
-                backward_f, back_residual = block(backward_f, back_residual, inference_params=None)
-            back_residual = (backward_f + back_residual) if back_residual is not None else backward_f
-
-            back_residual = torch.flip(back_residual, [1])
-            residual = torch.cat([residual, back_residual], -1)
-        
-        return residual
-"""
     
 
 class DenseBlock(nn.Module):
@@ -656,11 +425,11 @@ class PhaseDecoder(nn.Module):
 
 
 class TSMambaBlock(nn.Module):
-    def __init__(self):
+    def __init__(self, n_layer):
         super(TSMambaBlock, self).__init__()
 
-        self.time_mamba = MambaBlock(in_channels=64)
-        self.freq_mamba = MambaBlock(in_channels=64)
+        self.time_mamba = MambaBlock(in_channels=64, n_layer=n_layer)
+        self.freq_mamba = MambaBlock(in_channels=64, n_layer=n_layer)
 
         self.tlinear = nn.ConvTranspose1d(
             64 * 2, 64, 1, stride=1
@@ -669,6 +438,7 @@ class TSMambaBlock(nn.Module):
         self.flinear = nn.ConvTranspose1d(
             64 * 2, 64, 1, stride=1
         )
+        #LayerNormalization(emb_dim, dim=-3, total_dim=4, eps=eps),
 
     def forward(self, x):
         b, c, t, f = x.size()
@@ -684,6 +454,7 @@ class TSMambaBlock(nn.Module):
 class SEMamba(nn.Module):
     def __init__(self, h):
         super(SEMamba, self).__init__()
+        #self.num_tscblocks = 4
         self.num_tscblocks = 4
         self.dense_encoder = DenseEncoder(in_channel=2)
 
